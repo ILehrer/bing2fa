3b53ada19467ec0fdaabe3849664efad0357d676
Isaac Lehrer <zevilehrer@gmail.com>
2025-03-18 12:57:01 -0400

    support for setting storage

--- background.js
+++ background.js
@@ -1,9 +1,15 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "getStorage") {
-      chrome.storage.local.get("_bing_2fa_key", function(result) {
-        sendResponse(result);
-      });
-      return true; // Indicates asynchronous response
+	chrome.storage.local.get("_bing_2fa_key", function(result) {
+		sendResponse(result);
+	});
+	return true; // Indicates asynchronous response
     }
-  });
+    if (request.action === "setStorage") {
+	chrome.storage.local.set({"_bing_2fa_key": request.key}, function() {
+		sendResponse({success: true});
+	});
+	return true;
+    }
+});
   
